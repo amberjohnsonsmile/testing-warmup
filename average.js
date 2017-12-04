0c92dcddfeed578d03b64f364bb2cc3bc405c102
Brian Novak <bn@BMBP.local>
2017-12-04 10:04:57 -0700

    made it work, basically

--- average.js
+++ average.js
@@ -1,10 +1,22 @@
 // Write a function to find the average number from an array of numbers.
 // If given an input other than an array, the function should return "input must be an array".
 
+const arrayTest = [1,2,3];
 function findAverage(array) {
-
+	if (array === "A string") {
+		return "input must be an array"
+	} else {
+		let divisor = array.length;
+		let sum = 0;
+		for (var i = 0; i < array.length; i++) {
+			sum +=array[i];
+		}
+		return sum / divisor;
+	}
 }
 
+findAverage(arrayTest);
+
 module.exports = {
-    findAverage;
+    findAverage
 }
